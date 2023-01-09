0f01cc8bca58cb815df0df7d9da4c8f7c8c0d786
keimah507 <githinjibrian5@gmail.com>
2023-01-09 22:09:45 +0300

    Python Async function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -21,7 +21,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         delays.append(await t)
 
     return delays
-
-print(asyncio.run(wait_n(5, 5)))
-print(asyncio.run(wait_n(10, 7)))
-print(asyncio.run(wait_n(10, 0)))
