e234ea6b09ee3df58ffe7430d165fea25f83662e
keimah507 <githinjibrian5@gmail.com>
2023-01-10 22:33:47 +0300

    Python Async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[int, None, None]:
+    """Yields a random number between 0 and 10"""
     for _ in range(1, 10):
         await asyncio.sleep(1)
         r = random.uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Python Async comprehension task 1"""
 
-import asyncio
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
